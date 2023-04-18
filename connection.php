fbcbdca72d0328952190c1f91ebba30e7dabb3a3
Brenlify <robles.167967@gmail.com>
2023-04-18 08:02:16 +0800

    2nd connection edit

--- connection.php
+++ connection.php
@@ -13,3 +13,20 @@ if ($conn->connect_error) {
 }
 //echo "Connected successfully";
 ?>
+
+
+<?php
+// $servername = "localhost";
+// $username = "u235219407_Carpool2023";
+// $password = "Carpool2023";
+// $database = "u235219407_brencarpool";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $database);
+
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// //echo "Connected successfully";
+?>
